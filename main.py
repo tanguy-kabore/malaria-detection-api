f98451c81259f435d4d01692e71bc008941f2ba1
tanguy-kabore <baowendnere.kabore@gmail.com>
2024-10-12 00:24:33 +0000

    CORS modification

--- main.py
+++ main.py
@@ -281,4 +281,4 @@ def save_annotations():
     return jsonify({'message': 'Annotations et images sauvegardées avec succès'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
